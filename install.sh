9c30639e7b5cc329e8f9e2fb694eda6e1f0cf94d
Nico Verbruggen <nico.verb@gmail.com>
2019-10-13 14:44:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,3 +30,21 @@ valet link phpinfo
 
 # Cleanup
 sudo apt autoremove
+
+MYSQLPASS=$(openssl rand -base64 32)
+echo "$MYSQLPASS"
+
+# Make sure that NOBODY can access the server without a password
+sudo mysql -e "UPDATE mysql.user set authentication_string=PASSWORD('$MYSQLPASS') where user='root'"
+# Only allow local login
+sudo mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
+# Ensure we can login locally
+sudo mysql -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE User='root'"
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQLPASS'"
+# Make our changes take effect
+sudo mysql -e "FLUSH PRIVILEGES"
+
+# Create a file on the desktop
+touch "$HOME/Desktop/Passphrases.md"
+echo "Your MySQL password: $MYSQLPASS" | tee "$HOME/Desktop/Passphrases.md"
+echo "Your passphrases have been saved on your desktop. Store these in a safe location!"
